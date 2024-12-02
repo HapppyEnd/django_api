b0de47edda7f9ef8d3561b7acf5ad879f09687e9
Alena <sweetysweetygirl@gmail.com>
2024-12-02 12:37:38 +0300

    Modified permissions.

--- referal_system/api/permissions.py
+++ referal_system/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission
 
 class IsAdminOrIsSelf(BasePermission):
 
-    def has_permission(self, request):
+    def has_permission(self, request, view):
         if request.user and request.user.is_staff:
             return True
         return request.user and request.user.is_authenticated
